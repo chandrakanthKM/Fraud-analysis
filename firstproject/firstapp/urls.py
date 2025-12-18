944536e2a9178c543585441eb965486ec3c39e04
chandrakanthKM <123227009+chandrakanthKM@users.noreply.github.com>
2025-12-18 21:35:54 +0530

    Delete firstproject directory

--- firstproject/firstapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path("", views.index, name="index"),
-    path("post/<int:post_id>", views.Detail, name="Detailpage {post_id}"),
-	path('old-url/', views.old_url_view, name='old_url'),
-    path('new_something_url/', views.new_url_view, name='new_url'),]
--- firstproject/firstproject/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def custom_404_view(request, exception):
-    return render(request, 'firstproject/Template/404.html', status=404)
